87370b90892dce9309b4bd88d9f892cb3ab4b1d0
sushant1696 <shaan160196@gmail.com>
2020-02-13 18:56:39 +0530

    [Sushant]Add.added the usecase3

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,1 +1,11 @@
-echo "****Wellcome to the Employee wage system****"
+PartTimeEmployee(){
+	echo "enter a part time Employee "
+	read name
+	echo "Part time Employee name is :$name"
+	echo "enter the per hour wage"
+	read wage
+	totalhour=4
+	PertTimeWage=$(($wage*$totalhour))
+	echo "Total price of part time employee is =$PertTimeWage"
+}
+PartTimeEmployee
