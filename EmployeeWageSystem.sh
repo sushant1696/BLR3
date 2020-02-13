302e2e33cbd3fd3ab9dc4ea687ca10e9cfac58ec
sushant1696 <shaan160196@gmail.com>
2020-02-13 18:19:11 +0530

    [Sushant]Add.Adding the UseCase1

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,1 +1,11 @@
-echo "****Wellcome to the Employee wage system****"
+CheckPresentAbsent(){
+rand=$(($RANDOM%2))
+if(($rand<1))
+then
+echo "Sorry! Employee is absent"
+else
+echo "Employee is present"
+fi
+echo "$rand"
+}
+CheckPresentAbsent
