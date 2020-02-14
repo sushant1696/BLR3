140a22d7f0176c728f7be33c050cdfdb9a874d22
sushant1696 <shaan160196@gmail.com>
2020-02-14 09:46:56 +0530

    [Sushant]Add.added the usecase4

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,1 +1,43 @@
-echo "****Wellcome to the Employee wage system****"
+echo "1-> check attendance"
+echo "2-> daily employee wage"
+echo "3-> part time employee wage "
+value=$1
+case $value in 
+1 )
+CheckPresentAbsent(){
+rand=$(($RANDOM%2))
+if(($rand<1))
+then
+echo "Sorry! Employee is absent"
+else
+echo "Employee is present"
+fi
+echo "$rand"
+}
+CheckPresentAbsent
+;;
+
+2 )
+DailyEmployeeWage(){
+	PerHourWage=20
+	TotalHour=8
+	EmployeeWagePerDay=$(($PerHourWage*$TotalHour))
+	echo "Employee wage in a day is:$EmployeeWagePerDay"
+}
+DailyEmployeeWage
+;;
+
+3 )
+PartTimeEmployee(){
+	echo "enter a part time Employee "
+	read name
+	echo "Part time Employee name is :$name"
+	echo "enter the per hour wage"
+	read wage
+	totalhour=4
+	PertTimeWage=$(($wage*$totalhour))
+	echo "Total price of part time employee is =$PertTimeWage"
+}
+PartTimeEmployee
+;;
+esac
