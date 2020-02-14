4b145b99ec225495b9985f5f0a874014ae4ff9f1
sushant1696 <shaan160196@gmail.com>
2020-02-14 17:01:51 +0530

    [sushant]Add . added the usecase5

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,1 +1,18 @@
-echo "Wellcome to the Employee wage system"
+DailyEmployeeWage(){
+	PerHourWage=20
+	TotalHour=8
+	EmployeeWagePerDay=$(($PerHourWage*$TotalHour))
+	return $EmployeeWagePerDay
+	
+}
+DailyEmployeeWage
+echo "Employee wage in a day is:$EmployeeWagePerDay"
+WagePerMonth(){
+	day=20
+	DailyEmployeeWage
+	EmployeeWagePerDay=$?
+	monthlywage=$(($EmployeeWagePerDay * $day))
+	return $monthlywage
+}
+WagePerMonth
+echo "monthly wage is $monthlywage"
