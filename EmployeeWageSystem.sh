2bf4cad1f323d41f8dc93db4ea581412d78cde97
sushant1696 <shaan160196@gmail.com>
2020-02-14 18:16:34 +0530

    [sushant]Add .added the usecase7

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,1 +1,10 @@
-echo "Wellcome to the Employee wage system"
+ WorkHourCalculate()
+{
+	echo "enter the wage : "
+	read wage
+	workhours=$(( $wage / 20 ))
+	echo "work hour : $workhours"
+	return $workhours
+}
+WorkHourCalculate
+echo "employe work hour is
